1b89dc02ca3c0b550b028b27ec8c27e6060a58ea
H3nky <102431386+H3nky@users.noreply.github.com>
2024-07-11 12:02:05 -0700

    Added onEnabled and onDisable messages.

--- src/main/java/me/robomonkey/versus/Versus.java
+++ src/main/java/me/robomonkey/versus/Versus.java
@@ -1,17 +1,24 @@
 package me.robomonkey.versus;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Versus extends JavaPlugin {
 
+    public static void log(String message){
+        Bukkit.getServer().getLogger().info(message);
+    }
     @Override
     public void onEnable() {
         // Plugin startup logic
+        log("Versus has been enabled!");
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        log("Versus has been enabled!");
+
     }
 }
