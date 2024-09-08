da7425e9be6b92252bd886672065faf28b2020de
H3nky <102431386+H3nky@users.noreply.github.com>
2024-09-07 17:17:07 -0700

    Fixed Bstats dependency.

--- src/main/java/me/robomonkey/versus/duel/eventlisteners/InteractEventListener.java
+++ src/main/java/me/robomonkey/versus/duel/eventlisteners/InteractEventListener.java
@@ -7,16 +7,17 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.inventory.EquipmentSlot;
 
 public class InteractEventListener implements Listener {
 
     @EventHandler
     public void onInteractEvent(PlayerInteractAtEntityEvent event) {
         if (event.getRightClicked().getType() != EntityType.PLAYER) return;
-        if (!event.getPlayer().isSneaking()) return;
+        if (!event.getPlayer().isSneaking() || event.getHand() != EquipmentSlot.HAND) return;
         if (!Settings.is(Setting.SHIFT_CLICK_REQUESTING_ENABLED)) return;
         Player requested = (Player) event.getRightClicked();
         Player requesting = event.getPlayer();
-        requesting.performCommand("/duel " + requested.getName());
+        requesting.performCommand("duel " + requested.getName());
     }
 }
